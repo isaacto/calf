c898e0040ba223d683db7238f66f854e85e9ed92
Isaac To <isaacto@gmail.com>
2019-10-27 20:47:27 +0800

    docs: Add a couple of examples
    Signed-off-by: Isaac To <isaacto@gmail.com>

---
+++ docs/examples/composite.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python3
+
+"Show how to define a function with complex params as CLI using calf"
+
+import argparse
+import calf
+import collections
+import typing
+
+
+# Somehow you have this type...
+Name = collections.namedtuple('Name', ['firstname', 'lastname'])
+
+
+# And you need to support loading this type in your CLI functions...
+def hello(name: Name):
+    print(name)
+
+
+# So somewhere in your program you create your own selector
+class NameSelector(calf.LoaderSelector):
+    def match(self, param: str, ptype: type, kind: str,
+              info: typing.Optional[calf.ParamInfo]) -> bool:
+        return ptype == Name
+
+    def make_loader(self, param: str, ptype: type, kind: str,
+                    info: typing.Optional[calf.ParamInfo],
+                    default: typing.Any) -> calf.BaseArgLoader:
+        # Here we use a CompositeArgLoader.  Or you may define your own loader.
+        return calf.CompositeArgLoader(
+            param, ptype, info, default, Name,
+            [calf.OptArgLoader('firstname', str,
+                               calf.ParamInfo('First name', '-f'), 'Isaac'),
+             calf.OptArgLoader('lastname', str,
+                               calf.ParamInfo('Last name', '-l'), 'To'),])
+
+
+# Then define your own "call" function which is use the selector.
+def call(func):
+    calf.CalfRunner(
+        [NameSelector()],
+        doc_parser=calf.google_apidoc_parser,
+        param_parser=calf.basic_param_parser)(func)
+
+
+# Now your CLI is just as usual as you can imagine...
+if __name__ == '__main__':
+    call(hello)
---
+++ docs/examples/simple.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python3
+
+"Show how to define a function with simple params as CLI using calf"
+
+import argparse
+import calf
+import collections
+import typing
+
+
+GREETING = {
+    1: 'Cheers.',
+    2: 'Have a nice day.',
+    3: 'Nice to meet you.',
+}
+
+
+# Define your function just like other functions, but note the (-s) as
+# short option names, and {1, 2, 3} as choices.
+def hello(name: str = 'Isaac', *args, style = 2, **kwargs: float) -> None:
+    """Say hello
+
+    Args:
+
+        name: name of to say hello to
+
+        style: (-s) style, choose among {1, 2, 3}
+
+        args: values
+
+        kwargs: keyword values
+
+    """
+    print('Hello,', name)
+    print(GREETING[style])
+    print(args)
+    print(kwargs)
+
+
+# Your main program just use calf.call to call your CLI function.
+if __name__ == '__main__':
+    calf.call(hello)
