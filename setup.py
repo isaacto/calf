4013cc0b8cabce1df4a50627f1dd249b5675ab13
Isaac To <isaacto@gmail.com>
2019-11-02 21:32:13 +0800

    Add files needed for PyPI upload
    Signed-off-by: Isaac To <isaacto@gmail.com>

---
+++ setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+with open('README.md', 'r') as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='calf',
+    version='0.1',
+    author='Isaac To',
+    author_email='isaac.to@gmail.com',
+    description='Command Argument Loading Functions',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/isaacto/calf',
+    packages=setuptools.find_packages(),
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Programming Language :: Python :: 3.5',
+        'Topic :: Software Development :: Libraries',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+)
