d056b64338c926f0f7d2614c4be2737bbb03aacf
Isaac To <isaacto@gmail.com>
2019-11-04 22:08:00 +0800

    Add Python version tags

--- setup.py
+++ setup.py
@@ -6,6 +6,7 @@ with open('README.md', 'r') as fh:
 setuptools.setup(
     name='calf',
     version='0.1.5',
+    python_requires='~=3.5',
     author='Isaac To',
     author_email='isaac.to@gmail.com',
     description='Command Argument Loading Functions',
@@ -17,6 +18,9 @@ setuptools.setup(
     classifiers=[
         'Development Status :: 4 - Beta',
         'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
         'Topic :: Software Development :: Libraries',
         'License :: OSI Approved :: MIT License',
         'Operating System :: OS Independent',
