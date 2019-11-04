f9aeb23f90f0e9f3caf3ced9f2dd1abf8c333cc7
Isaac To <isaacto@gmail.com>
2019-11-04 21:13:01 +0800

    Add py.typed marker file for type checking
    Signed-off-by: Isaac To <isaacto@gmail.com>

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ setuptools.setup(
     long_description_content_type='text/markdown',
     url='https://github.com/isaacto/calf',
     packages=setuptools.find_packages(),
-    package_data={'calf': ['docs/*']},
+    package_data={'calf': ['py.typed']},
     classifiers=[
         'Development Status :: 4 - Beta',
         'Programming Language :: Python :: 3.5',
